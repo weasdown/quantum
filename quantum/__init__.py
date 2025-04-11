f880874e884c79d5a8832f5537940c730c5b73f9
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-11 10:29:12 +0100

    Create Python package (#3)
    * feat: add docs and tests folders
    
    * docs: update README.md to use template from https://www.makeareadme.com/
    
    * feat: add __init__.py
    
    * feat: add dirac.py
    
    * feat: update README.md to use template from https://github.com/othneildrew/Best-README-Template/blob/main/BLANK_README.md
    
    * feat: add information.py

---
+++ quantum/__init__.py
@@ -0,0 +1,3 @@
+# A Python library for representing quantum computing.
+
+from .information import ClassicalInformation, QuantumInformation
---
+++ quantum/dirac.py
@@ -0,0 +1,3 @@
+# For representing Dirac notation, also known as Bra-ket notation (https://en.wikipedia.org/wiki/Bra%E2%80%93ket_notation).
+# See also IBM's training series "Understanding Quantum Information & Computation", that covers Dirac notation in lesson 01 (https://youtu.be/3-c4xJa7Flk?si=0w9c-zsUs1REQveo&t=652).
+
