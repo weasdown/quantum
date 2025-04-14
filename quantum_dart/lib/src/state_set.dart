4d03f500800ef150ed5ee0d711ed31f93d0d3981
weasdown <34220924+weasdown@users.noreply.github.com>
2025-04-14 10:41:13 +0100

    feat: squash merge state_set_dart

--- quantum_dart/lib/src/state_set.dart
+++ quantum_dart/lib/src/state_set.dart
@@ -1,11 +1,21 @@
 import 'dart:collection';
 
+import 'package:matrix/vector.dart';
+
 // TODO add subtypes for classical states and probabilistic states.
-class State extends Object {}
+class State extends Object {
+  const State({required this.name, required this.symbol});
+
+  final String name;
+
+  final String symbol;
+}
 
 /// A [Set] containing [State]s.
+///
+/// The order of the elements of the set will always be the order they were given in when the [StateSet] was first constructed.
 class StateSet extends SetBase<State> {
-  StateSet({required Set<State> states}) : _states = states;
+  StateSet({required Set<State> states}) : _states = Set.unmodifiable(states);
 
   @override
   bool add(State state) {
@@ -17,12 +27,37 @@ class StateSet extends SetBase<State> {
     }
   }
 
+  /// Returns the column vector with a 1 in the entry corresponding to [state], with 0 for all other entries.
+  ColumnVector bra(State state) {
+    int stateIndex = _states.toList().indexOf(state);
+    // indexOf did not find the state in _states.
+    if (stateIndex == -1) {
+      throw ArgumentError('The State $state is not in the StateSet.');
+    }
+
+    ColumnVector vector = ColumnVector.filled(
+      length: _states.length,
+      filler: 0,
+    );
+    vector[stateIndex] = state;
+
+    // FIXME ColumnVector class not yet sufficiently implemented
+    // return vector;
+
+    // TODO remove UnimplementedError.
+    throw UnimplementedError('StateSet.bra() is not yet implemented.');
+  }
+
   @override
   bool contains(Object? element) =>
       (element is! State) ? false : _states.contains(element);
 
   @override
   Iterator<State> get iterator => _states.iterator;
+
+  /// Returns the row vector with a 1 in the entry corresponding to [state], with 0 for all other entries.
+  RowVector ket(State state) =>
+      throw UnimplementedError('StateSet.ket() is not yet implemented.');
 
   @override
   int get length => states.length;
